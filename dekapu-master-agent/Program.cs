62a50928f33ee92317e61466813905c1f257bc95
njm2360 <me@njm2360.com>
2026-01-08 21:09:28 +0900

    Add prevent duplicate launch

--- dekapu-master-agent/Program.cs
+++ dekapu-master-agent/Program.cs
@@ -2,18 +2,35 @@ namespace dekapu_master_agent
 {
     internal static class Program
     {
+        private static Mutex? _mutex;
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            const string mutexName = "dekapu_master_agent_single_instance";
+
+            _mutex = new Mutex(true, mutexName, out bool createdNew);
+
+            if (!createdNew)
+            {
+                MessageBox.Show(
+                    "多重起動を検知しました",
+                    "起動エラー",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                return;
+            }
+
             ApplicationConfiguration.Initialize();
 
             var config = AppConfig.Load("config.json");
             Application.Run(new TrayAppContext(config));
+
+            _mutex.ReleaseMutex();
         }
     }
 }
